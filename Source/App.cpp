4f257aa0c642c4e4d9a8b41fb0284fbddca4a58e
ElectronicsArchiver <85485984+ElectronicsArchiver@users.noreply.github.com>
2022-07-03 02:03:53 -0400

    Converted To PlatformIO & Replaced SR04 Lib

--- Source/App.cpp
+++ Source/App.cpp
@@ -1,4 +1,4 @@
-#include <SR04.h>
+#include <Ultrasonic.h>
 #include <IRremote.h>
 // #include <SimpleDHT.h>
 
@@ -24,9 +24,10 @@
 
 using ul = unsigned long;
 
+
 struct eztm {
 
-    ul second , minute , hour;
+    ul second , minute , hour ;
 
     eztm(ul millis){
 
@@ -38,7 +39,6 @@ struct eztm {
         minute %= 60;
         hour %= 24;
     }
-
 };
 
 // constant define
@@ -52,7 +52,9 @@ const int
     IRreceiver = 11 ,       //  Signal Pin of IR receiver to Arduino Digital Pin 11
     clock = 10 ,            //  74HC595 pin 10 SHCP
     latch = 9 ,             //  74HC595 pin 9 STCP
-    data = 8 ,              //  74HC595 pin 8 DS
+    data = 8 ;              //  74HC595 pin 8 DS
+
+int
     echo = 7 ,
     trig = 6 ;
 
@@ -90,7 +92,7 @@ ul initialtime;
 
 // Set up ultra sonic sensor and IR sensor
 
-SR04 sr04 = SR04(echo,trig);
+Ultrasonic sr04 ( echo , trig );
 IRrecv irrecv(IRreceiver);
 decode_results results;
 
@@ -303,7 +305,7 @@ void loop(){
 
     // Fetch distance
 
-    long distance = sr04.Distance();
+    long distance = sr04.Ranging(CM);
 
     print(distance);
     println("cm");
